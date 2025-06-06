97bcd04fca4b24267f14b65dd7f542c2e4a3aaf5
alwaysjeevanr <jeevanjeevu233@gmail.com>
2025-06-06 21:14:25 +0530

    updated .env

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://job-recruitment-backend-mv8y.onrender.com'
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 API.interceptors.request.use(
